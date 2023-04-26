962c8f6d445a1c171c7aa7e8b515478c0593df3a
LeoHacopian <leo.hacopian.514@my.csun.edu>
2023-04-26 00:04:26 -0700

    Adds Text Overlay to Drone Image

--- src/Components/GameSelection/GameSelection.js
+++ src/Components/GameSelection/GameSelection.js
@@ -5,7 +5,12 @@ export default function GameSelection() {
     return (
         <div className='GameSelection-CompleteContainer'>
             <img src='https://www.wallpaperflare.com/static/898/70/439/helicopter-sunset-silhouette-sky-wallpaper.jpg'className='GameSelection-Image' />
-            <div className="Text-Overlay">Text</div>
+            <div className="Text-Overlay Jost">
+                <h1>Drone Decisions</h1>
+                <h2>WHEN SHOULD ARMED DRONES BE USED?</h2>
+                <h6>AVERAGE TIME: 8 MIN </h6>
+            </div>
+            <button className='GameSelection-Button Jost'>PLAY</button>
         </div>
     )
 }
